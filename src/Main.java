a3b063e8b2b8cf79c5a68730cfebd420e014d451
flvtisv <flatislove@gmail.com>
2022-09-14 19:22:41 +0600

    task2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+//Напишите программу на Java для переворота последовательности символов
+// в строке без использования встроенной в String функции reverse().
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str = "reverse string";
+        char[] symbols = str.toCharArray();
+        for (int i = symbols.length-1; i >=0 ; i--) {
+            System.out.print(symbols[i]);
+        }
     }
 }
