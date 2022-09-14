e4455653d0b3ff3039a213c995fef478b00f044f
flvtisv <flatislove@gmail.com>
2022-09-14 21:51:46 +0600

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
+//Напишите программу на Java для того, чтобы поменять местами значения,
+// хранящиеся в двух переменных с помощью третьей переменной
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a,b,temp;
+        System.out.println("Введите значения a и b");
+        Scanner scanner = new Scanner(System.in);
+        a=scanner.nextInt();
+        b=scanner.nextInt();
+        System.out.println("a="+a+" b="+b);
+        temp=a;
+        a=b;
+        b=temp;
+        System.out.println("Result: a="+a+" b="+b);
     }
 }
