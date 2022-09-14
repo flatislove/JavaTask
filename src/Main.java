c01346fe0ff5bc95ecf557410ed5179a1a4c6a1d
flvtisv <flatislove@gmail.com>
2022-09-15 00:54:48 +0600

    task7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
+//Напишите программу на Java, чтобы узнать, является ли число простым или нет.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int temp,number;
+        boolean numberPrime=true;
+        Scanner scanner=new Scanner(System.in);
+        number=scanner.nextInt();
+        scanner.close();
+        for (int i = 2; i <=number/2 ; i++) {
+            if(number%i==0){
+                numberPrime=false;
+                break;
+            }
+        }
+        if (numberPrime){
+            System.out.println(number+" простое");
+        }
+        else System.out.println(number+" не простое");
     }
 }
