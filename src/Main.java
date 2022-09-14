4d5602a701a7563d5bdaafcd5e68ad96ee322cb3
flvtisv <flatislove@gmail.com>
2022-09-14 22:57:00 +0600

    task5

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.HashMap;
+
+//Напишите программу на Java для подсчета количества
+// конкретных слов в строке, используя HashMap.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str="word1 word2 word2 word3 word5 word2";
+        String[] words = str.split(" ");
+        HashMap<String,Integer> wordsMap = new HashMap<String,Integer>();
+        for (int i = 0; i <=words.length-1 ; i++) {
+            if(wordsMap.containsKey(words[i])){
+                int counter = wordsMap.get(words[i]);
+                wordsMap.put(words[i],counter+1);
+            }
+            else {
+                wordsMap.put(words[i],1);
+            }
+        }
+        System.out.println(wordsMap);
     }
 }
