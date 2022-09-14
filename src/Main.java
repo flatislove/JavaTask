e4fbe281aa08094d6f35461011d48bf8cb9b3edc
flvtisv <flatislove@gmail.com>
2022-09-14 21:44:22 +0600

    task2_2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+//Напишите программу на Java для переворота последовательности символов
+// в строке без использования встроенной в String функции reverse().
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String inputString, reverseString = "";
+        System.out.println("Введите строку");
+        Scanner scanner = new Scanner(System.in);
+        inputString=scanner.nextLine();
+        for (int i = inputString.length()-1; i>=0 ; i--) {
+            reverseString+=inputString.charAt(i);
+        }
+        System.out.println(reverseString);
     }
 }
