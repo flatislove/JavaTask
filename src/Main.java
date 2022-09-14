979a5842336608fcb8e00e530f3bf51ea3c2c741
flvtisv <flatislove@gmail.com>
2022-09-15 01:09:48 +0600

    task8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
+//Напишите Java-программу, чтобы определить, является ли строка
+// или число палиндромом, или нет.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String input,reverse="";
+        Scanner scanner = new Scanner(System.in);
+        int stringLengt;
+        System.out.println("Введите число или строку");
+        input = scanner.nextLine();
+        stringLengt=input.length();
+        for (int i = stringLengt-1; i >=0 ; i--) {
+            reverse += input.charAt(i);
+        }
+        if (input.equals(reverse)){
+            System.out.println("палиндром");
+        }
+        else System.out.println("не палиндром");
     }
 }
