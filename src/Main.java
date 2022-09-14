e695da5f0801dcacbfeaf754a7ade3ea0dd73b5b
flvtisv <flatislove@gmail.com>
2022-09-14 21:33:16 +0600

    task2_1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String str;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку");
+        str = scanner.nextLine();
+        String[] temp = str.split("");
+        for (int i = temp.length-1; i >=0 ; i--) {
+            System.out.print(temp[i]+"");
+        }
     }
 }
