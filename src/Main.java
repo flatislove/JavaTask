db178339b59bd7f5b21b4f2df664d98b4f203c76
flvtisv <flatislove@gmail.com>
2022-09-14 22:39:14 +0600

    task4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
+//Напишите программу на Java, чтобы поменять местами значения, хранящиеся
+// в двух переменных, без использования третьей переменной.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int a,b;
+        Scanner scanner = new Scanner(System.in);
+        a=scanner.nextInt();
+        b=scanner.nextInt();
+        System.out.println("a="+a+" b="+b);
+        a+=b;
+        b=a-b;
+        a-=b;
+        System.out.println("a="+a+" b="+b);
     }
 }
