be1115eb06085e70fb0f2fd6f9dddb654863a573
flvtisv <flatislove@gmail.com>
2022-09-14 19:09:36 +0600

    task1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+//Напишите программу на Java для переворачивания строки,
+// изменив расположение символов в строке задом наперёд
+// без использования встроенных в String функций.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String str="text string";
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(str);
+        stringBuilder=stringBuilder.reverse();
+        System.out.println(stringBuilder);
     }
 }
