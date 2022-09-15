2d5b79d30261c258716fb8a3de62b66af65c4cac
flvtisv <flatislove@gmail.com>
2022-09-15 22:55:17 +0600

    task9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
+//Написать программу на Java для вычисления серии чисел Фибоначчи.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int number,x=0,y=0,z=1;
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Введите количество значений");
+        number=scanner.nextInt();
+        for (int i = 0; i <= number; i++) {
+            x=y;
+            y=z;
+            z=x+y;
+            System.out.println(x+" ");
+        }
     }
 }
