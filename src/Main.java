6434fe61bbdad14a12b83d69e0429db7bfd0561e
flvtisv <flatislove@gmail.com>
2022-09-15 23:30:41 +0600

    task11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+//Напишите программу на Java, чтобы найти повторяющиеся символы в строке.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       String str = new String("textstring");
+       int counter = 0;
+       char[] chars = str.toCharArray();
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i+1; j <str.length() ; j++) {
+                if (chars[i]==chars[j]){
+                    System.out.println(chars[i]);
+                    counter++;
+                    break;
+                }
+            }
+        }
     }
 }
