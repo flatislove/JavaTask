c2a9ab55ec9da32e88020db635754d9b4aa354d4
flvtisv <flatislove@gmail.com>
2022-10-01 18:52:59 +0600

    task15

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+//Write a function that takes an integer as input, and returns the number of bits that are equal
+// to one in the binary representation of that number. You can guarantee that input is non-negative.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(countBitBinary(1234));
+    }
+    public static int countBitBinary(int number){
+        String s= Integer.toBinaryString(number);
+        int count=0;
+        char[] chars = s.toCharArray();
+        for (char aChar : chars) {
+            if (aChar == '1') count++;
+        }
+        return count;
     }
 }
