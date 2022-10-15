bee62ee7be77347dd12ecdbbcf2f6319cf648ded
flvtisv <flatislove@gmail.com>
2022-10-15 14:57:26 +0600

    task22

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,7 @@
+//The rgb function is incomplete. Complete it so that passing in RGB decimal values will result in a hexadecimal
+// representation being returned. Valid decimal values for RGB are 0 - 255. Any values that fall out of that range
+// must be rounded to the closest valid value. Note: Your answer should always be 6 characters long, the shorthand
+// with 3 will not work here.
 public class Main {
     public static void main(String[] args) {
         System.out.println(rgb(0,34,21));
